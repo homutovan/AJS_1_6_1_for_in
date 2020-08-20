d69e7a7219091f01682ae47fe3ac031808a57405
Andrey Khomutov <homutovan@live.com>
2020-08-20 23:33:02 +0700

    Add functions & tests

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# js env starter
+[![Build status](https://ci.appveyor.com/api/projects/status/nu0loim99cyj5mp3/branch/master?svg=true)](https://ci.appveyor.com/project/homutovan/ajs-1-6-1-for-in/branch/master)
 
-[![Build status](https://ci.appveyor.com/api/projects/status/pgje30jtk2oevnyy/branch/master?svg=true)](https://ci.appveyor.com/project/homutovan/starter/branch/master)
+# Домашнее задание к лекции «Object, Reflection и Proxy»
+
+## for ... in
 
 ### Installation
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,12 @@
-console.log('app worked');
+export default function sortProp(obj, order) {
+  const objList = [];
+  const specObjList = [];
+  order.forEach((key) => specObjList.push({ key, value: obj[key] }));
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      if (!order.includes(key)) objList.push({ key, value: obj[key] });
+    }
+  }
+  objList.sort((a, b) => (a.key < b.key && -1) || (a.key > b.key && 1) || 0);
+  return [...specObjList, ...objList];
+}
